bf0362ad36ae7974181efa72bc1ae0a2ee39f20d
Dr. H.Wang <wanghan79@gmail.com>
2022-07-03 01:19:08 +0800

    Fixed TextReader1.0 to TXTReader Fixed SplitFile in MethylationDataProcess.py

--- MethylationEvaluation/MethylationDataProcess.py
+++ MethylationEvaluation/MethylationDataProcess.py
@@ -1,43 +1,41 @@
+from TXTReader import TXTReader
+
 
 class MethylationDataProcess:
     """
     Description:
     """
 
-    def StaticData(self, file: str, statSubjects="ALL", tofile=False):
+    def StaticData(self, file: str, flag="ALL", tofile=False):
         """
         Description:
         :return: Default outputs the st
         :param file:  The Methylation source data file full path
-        :param statSubjects: If the caller requires outputting the statistics results, it should be an iterable
+        :param flag: If the caller requires outputting the statistics results, it should be an iterable
                        object containing a group of strings, such as: "MissingValues", "MissingElements", "ErrValues"
         :param tofile: Default as False, set to True when the statical results are required storing in files
         :return: The statical results or the corresponding files
         """
-        if txt:
-            data = __statTXT(file)
-        elif csv:
-            readcsv
-        elif .gz:
-            0000
+        pass
 
-        for item in data:
-
-        return outputfiles
-
-    def __statTXT(self, file):
-        return iterable
-
-    def SplitFile(self, file: str, rows, columns, output_path: str):
+    def SplitFile(self, file: str, destpath: str, rows: range, cols: range, col_delimiter: str = r" ") -> str:
         """
-        Description:
-        :param file: The full path of the file to be split
-        :param rows: The rows for each sub-file, it should allow the inputs as: row number, row labels, or row indexes
-        :param columns: The columns for each sub-file, it should allow the inputs as parameter "rows"
-        :param output_path: A fold to output the result files
-        :return: True-succeed, False-failed
+        :param file: The file to be split
+        :param destpath: The folder to keep sub-files
+        :param rows:
+        :param cols:
+        :param col_delimiter:
+        :return:
         """
-        return False
+        try:
+            matrix = TXTReader.IterReadMatrix(file, rows, cols, col_delimiter)
+        except Exception as e:
+            raise e
+        try:
+            self.__write_to_file(destpath, matrix)
+        except Exception as e:
+            raise e
+        return destpath
 
     def FillMissingValues(self, file: str, missing_values, filling_values, outfile: str):
         """
@@ -48,7 +46,7 @@ class MethylationDataProcess:
         :param outfile: Full path of the result file to be created
         :return: True-succeed, False-failed
         """
-        return False
+        pass
 
     def FillMissingElements(self, file: str, missing_elems, filling_elems, outfile: str):
         """
@@ -59,4 +57,11 @@ class MethylationDataProcess:
         :param outfile: Full path of the result file to be created
         :return: True-succeed, False-failed
         """
-        return False
+        pass
+
+    def __generate_subfile_name(self, file, index):
+        pass
+
+    # This function should be in the package of file utility
+    def __write_to_file(self, file: str, content: iter) -> list:
+        pass
---
+++ MethylationEvaluation/test_TXTReader.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author   : H.Wang
+# @Time     : 2022/7/3 0:25
+# @File     : test_TXTReader.py
+from unittest import TestCase
+
+
+# @Project  : MethylationEvaluation
+class TestTxtFileReader(TestCase):
+	def test_iter_read(self):
+		self.fail()
+
+	def test_iter_read_matrix(self):
+		self.fail()
