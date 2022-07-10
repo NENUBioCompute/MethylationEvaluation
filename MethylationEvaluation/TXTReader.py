ddbf083a5267596b85e5d48fc22a056ea006a399
Cecilia <cairuirui0416@gmail.com>
2022-07-11 06:45:53 +0800

    Modify exceptions.

--- MethylationEvaluation/TXTReader.py
+++ MethylationEvaluation/TXTReader.py
@@ -19,22 +19,18 @@ class TXTReader:
         """
         count = 0
         lines = []
-        try:
-            with open(file, 'r') as f:
-                for aline in f:
-                    count += 1
-                    lines.append(aline.strip())
-                    if count >= num_read:
-                        yield lines
-                    # Free buffer zone
-                        count = 0
-                        lines.clear()
-            # yield the reset lines counted less than num_read
-                if count > 0:
+        with open(file, 'r') as f:
+            for aline in f:
+                count += 1
+                lines.append(aline.strip())
+                if count >= num_read:
                     yield lines
-        except IOError:
-            ioError = "Sorry, the file " + file + " does not exist."
-            print(ioError)
+                    # Free buffer zone
+                    count = 0
+                    lines.clear()
+            # yield the reset lines counted less than num_read
+            if count > 0:
+                yield lines
 
     @staticmethod
     def IterReadMatrix(file: str, rows: range, cols: range, col_delimiter: str = r" ") -> iter:
@@ -45,73 +41,38 @@ class TXTReader:
         :param col_delimiter: the delimiter/s may be used in the text file, examples: r"[ \n\t;,!]"
 
         """
-        try:
-            with open(file, 'r') as f:
+        with open(file, 'r') as f:
+            # if rows is out of range
+            file_len = TXTReader.FileLen(file)
+            if rows.start < rows.stop <= file_len:
                 for i in range(rows.start):
                     next(f)
-            try:
                 for i in range(rows.start, rows.stop):
                     aline = f.readline()
                     if not aline:
                         break
                     elements = re.split(col_delimiter, aline)
-                    yield elements[cols.start:cols.stop]
-            except IndexError:
-                print("Please input the correct range!")
-        except IOError:
-            ioError = "Sorry, the file " + file + " does not exist."
-            print(ioError)
+                    col_len = len(elements)
+                    #if cols is out of range
+                    if cols.start < cols.stop <=col_len:
+                        yield elements[cols.start:cols.stop]
+                        col_len_temp = len(elements[cols.start:cols.stop])
+                        # if it is a matrix
+                        if col_len != col_len_temp:
+                            raise Exception("Not a Matrix!")
+                    else:
+                        raise Exception("Please input the correct columns range!")
+            else:
+                raise Exception("Please input the correct row range!")
 
-    # def split_row(self, line_count):
-    #     for i in range(int(len(self.context) / line_count)):
-    #         self.write_file(i, i, self.context[i:i + line_count])
-    #
-    #
-    # def split_column(self, column_count):
-    #     df = pd.DataFrame(self.context)
-    #     print(df.shape[1])
-    #     for i in range(int(df.shape[1] / column_count)):
-    #         temp_list = np.array(df[range(i, i + column_count)]).tolist()
-    #         for j in range(len(temp_list)):
-    #             temp_list[j] = list(filter(None, temp_list[j]))
-    #         self.write_file(i, i, temp_list)
-
-    # def get_part_file_name(self, part_num, temp_count):
-    #     """"获取分割后的文件名称：在源文件相同目录下建立临时文件夹temp_part_file，然后将分割后的文件放到该路径下"""
-    #     temp_path = os.path.dirname(self.file)  # 获取文件的路径（不含文件名）
-    #     temp_name = os.path.splitext(os.path.basename(self.file))[0]
-    #     part_file_name = temp_path + os.sep + temp_name
-    #     if not os.path.exists(part_file_name):  # 如果临时目录不存在则创建
-    #         os.makedirs(part_file_name)
-    #     part_file_name += os.sep + temp_name + "_part" + str(part_num) + "_" + str(temp_count) + ".txt"
-    #     return part_file_name
-
-    # def write_file(self, part_num, temp_count, *line_content):
-    #     """
-    #
-    #     """
-    #     part_file_name = self.get_part_file_name(part_num, temp_count)
-    #     try:
-    #         with open(part_file_name, "w") as part_file:
-    #             for i in line_content[0]:
-    #                 part_file.writelines(i)
-    #                 part_file.write("\n")
-    #
-    #     except IOError as err:
-    #         print(err)
-
-    # def write_file(self, part_num, temp_count, *line_content):
-    #     """将按行分割后的内容写入相应的分割文件中"""
-    #     # print(temp_count)
-    #     part_file_name = self.get_part_file_name(part_num, temp_count)
-    #     try:
-    #         with open(part_file_name, "w") as part_file:
-    #             for i in line_content[0]:
-    #                 part_file.writelines(i)
-    #                 part_file.write("\n")
-    #
-    #     except IOError as err:
-    #         print(err)
+    @staticmethod
+    def FileLen(file:str):
+        File = TXTReader.IterRead(file, 1)
+        content = []
+        for aline in File:
+            content.append(aline)
+        File_len =len(content)
+        return File_len
 
 
 if __name__ == "__main__":
@@ -126,3 +87,10 @@ if __name__ == "__main__":
     # result = test.IterReadMatrix("TXT_test.txt", range(0, 3), range(0, 2), r"[ \n]")
     # for line in result:
     #     print(line)
+    #
+    # file_len = TXTReader.FileLen("E:/net download/GSE62867_series_matrix.txt")
+    # print(file_len)
+    file = TXTReader.IterReadMatrix("E:/net download/GSE62867_series_matrix.txt", range(46, 65), range(0, 5), r"[ \n\t]")
+    for line in file:
+        print(line)
+
